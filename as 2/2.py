9d670f2ed8c2b1cb30ce6e8fe815e32a82ee73d7
Rohan-trivedi-13 <121244272+Rohan-trivedi-13@users.noreply.github.com>
2022-12-22 21:55:22 +0530

    Update 2.py

--- as 2/2.py
+++ as 2/2.py
@@ -1,5 +1,5 @@
-name = "akshat kesari"
-SID = "22106050"
+name = "rohan trivedi"
+SID = "22106044"
 department = " Data Science "
 CGPA = 9.9
 print("Hey,", name, "Here!\nMy SID is", SID, "\nI am from", department, "department and my CGPA is", CGPA)
