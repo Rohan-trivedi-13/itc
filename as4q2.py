9860456bd31d3105e782837114a81206a8c34020
Rohan-trivedi-13 <121244272+Rohan-trivedi-13@users.noreply.github.com>
2023-01-14 23:09:09 +0530

    Delete as4q2.py

--- as4q2.py
+++
@@ -1,9 +0,0 @@
-year=int(input("enter year: "))
-if year<0:
-    print("invalid")
-elif year%100==0 and year%400==0:
-    print("leap year")
-elif year%4==0 and year%100!=0:
-    print("leap year")
-else:
-    print("not a leap year")
