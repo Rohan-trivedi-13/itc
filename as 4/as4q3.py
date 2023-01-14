f695bc04ec50e00d0cd6dbd3248a55a17691d114
Rohan-trivedi-13 <121244272+Rohan-trivedi-13@users.noreply.github.com>
2023-01-14 23:35:22 +0530

    Add files via upload

---
+++ as 4/as4q3.py
@@ -0,0 +1,16 @@
+a=()
+b=()
+c=()
+import random
+for i in range(1,11):
+    a=random.randint(0,9)
+    b=random.randint(0,9)
+    c=a*b
+    print("Question",i,":",a,"*",b,"=")
+    d=int(input("enter your answer: "))
+    if d==c:
+        print("Right!")
+    else:
+        print("Wrong. The answer is",c)
+    i+=1
+
