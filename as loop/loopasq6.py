7decc7fe481d0f99134d04fb801a927dccbc494b
Rohan-trivedi-13 <121244272+Rohan-trivedi-13@users.noreply.github.com>
2023-01-15 02:47:45 +0530

    Update loopasq6.py

--- as loop/loopasq6.py
+++ as loop/loopasq6.py
@@ -1,10 +1,12 @@
 #q6
-for i in range(2,int(input("Enter upper limit: "))):
+a=int(input("enter lower limit"))
+for i in range(a,int(input("Enter upper limit: "))):
     for j in range(2,i):
         if i%j==0:
             break
     else:
         print(i)
+
 
             
         
