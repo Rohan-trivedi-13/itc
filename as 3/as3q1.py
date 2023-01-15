291949f4831e62c67a4b66ec47292095ef3a91d0
Rohan-trivedi-13 <121244272+Rohan-trivedi-13@users.noreply.github.com>
2023-01-15 16:27:56 +0530

    Add files via upload

---
+++ as 3/as3q1.py
@@ -0,0 +1,14 @@
+str1=input("Enter string: ")
+lst1=list(str1.split(" "))
+lst2=list(set(lst1))
+for i in lst2:
+    if len(lst1)!=1:
+        print(i,"occurs",lst1.count(i),"times")
+    else:
+        lst3=list(lst1[0])
+        lst4=list(set(lst3))
+        for j in lst4:
+            print(j,"occurs",lst3.count(j),"times")
+    
+
+
